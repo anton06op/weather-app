1cd6eccb248aa0b7245889e65b4fe15a516a54a1
anton06op <anton06op@yandex.ru>
2023-03-25 01:35:31 +0200

    add searchform

---
+++ src/Body/SearchBar.js
@@ -0,0 +1,17 @@
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import SearchForm from './SearchForm';
+
+function SearchBar({ show, handleClose }) {
+    return (
+        <Offcanvas show={show} onHide={handleClose}>
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Search</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                <SearchForm />
+            </Offcanvas.Body>
+        </Offcanvas>
+    );
+}
+
+export default SearchBar;
--- src/Body/index.js
+++ src/Body/index.js
@@ -1,27 +1,43 @@
-
+import { useState } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import Button from 'react-bootstrap/Button';
 import TimeSelector from './TimeSelector';
 import Map from './Map';
 import './body.scss';
+import SearchBar from './SearchBar';
 
 function Body() {
+
+  const [showBar, setShowBar] = useState(false);
+
+  const handleCloseBar = () => setShowBar(false);
+  const handleShowBar = () => setShowBar(true);
+
   return (
-    <Tabs
-      defaultActiveKey="now"
-      id="justify-tab-example"
-      className="mb-3"
-      justify
-    >
-      <Tab eventKey="now" title="Now">
-        <TimeSelector id="now" />
-        <Map />
-      </Tab>
-      <Tab eventKey="forecast" title="Forecast">
-        <TimeSelector id="forecast" />
-        <Map />
-      </Tab>
-    </Tabs>
+    <>
+      <Button className="mb-4" variant="primary" onClick={handleShowBar}>
+        Search
+      </Button>
+
+      <SearchBar show={showBar} handleClose={handleCloseBar} />
+
+      <Tabs
+        defaultActiveKey="now"
+        id="justify-tab-example"
+        className="mb-3"
+        justify
+      >
+        <Tab eventKey="now" title="Now">
+          <TimeSelector id="now" />
+          <Map />
+        </Tab>
+        <Tab eventKey="forecast" title="Forecast">
+          <TimeSelector id="forecast" />
+          <Map />
+        </Tab>
+      </Tabs>
+    </>
   );
 }
 
